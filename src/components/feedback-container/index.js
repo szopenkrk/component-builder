6ced7834b2511d12b5d08f0213797c0afd2efe20
Karol Kochaski <karolkochanski@gmail.com>
2020-01-19 19:00:28 +0100

    Add updates

--- src/components/feedback-container/index.js
+++ src/components/feedback-container/index.js
@@ -16,12 +16,19 @@ class FeedbackContainer extends Component {
             <div className="grid">
                 <div className="box box1">
                     <div className='label'>NET PROMOTER SCORE DETAILS</div>
+                    <div className={'feedback-value'}>
+                        + 32
+                    </div>
+                    <div className={'feedback-legend'}>
+                        NPS = % PROMOTERS - % DETRACTORS
+                    </div>
                 </div>
                 <div className="box box2">
                     <div className='label'>ALL CONSUMERS</div>
                 </div>
                 <div className="box box4">
                     <div className='label'>FEEDBACK</div>
+                    <div className={'feedback-line'}></div>
                 </div>
             </div>
         </div>
