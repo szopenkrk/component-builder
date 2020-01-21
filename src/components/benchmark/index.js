0657e62030f70b59ea4c39dfec74fbb6291723fd
Karol Kochaski <karolkochanski@gmail.com>
2020-01-22 00:06:22 +0100

    add bar

--- src/components/benchmark/index.js
+++ src/components/benchmark/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import './style.css';
 
 class Benchmark extends Component {
@@ -9,9 +9,31 @@ class Benchmark extends Component {
     }
 
     render() {
+        const data = {
+            labels: ['January', 'February'],
+            datasets: [
+                {
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    tooltips: {},
+                    borderWidth: 0,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [65, 59, ]
+                }
+            ]
+        };
         return (
             <div className="benchmark-container">
                 <div className={'benchmark-label'}>BENCHMARK</div>
+                <Bar
+                    data={data}
+                    width={100}
+                    height={50}
+                    options={{
+                        maintainAspectRatio: false
+                    }}
+                />
             </div>
         );
     };
